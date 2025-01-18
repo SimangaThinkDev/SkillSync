87e928950204280c846c376b55053d9bfc2e6dc3
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-01-18 14:37:42 +0200

    added file that will handle authentication &signup
    This file will use firebase to:
        -Create a user and store their information online
        -Authenticate User so they Can Use SkillSync

---
+++ authentication_and_signup.py
@@ -0,0 +1,47 @@
+import pyrebase
+import time
+from key import firebase
+
+
+
+
+"""
+Create an Authentication Object
+"""
+
+def authenticate_email():
+    """
+    Authentication of email
+    """
+
+    auth = firebase.auth()
+
+    print("WELCOME TO LOGIN PAGE\n\n")
+    time.sleep(1.5)
+    email = input("Enter your Username: ")
+    password = input("Enter your password: ")
+    try:
+        auth.sign_in_with_email_and_password(email, password)
+        print(f"Welcome back to Skillsync {email}")
+    except:
+        print("Account or Email does not exist")
+
+def create_account():
+    """
+    Email Sign-Up
+    """
+    print("WELCOME TO SIGN UP PAGE\n\n")
+    auth = firebase.auth()
+    time.sleep(1.5)
+    
+    while True:
+        email = input("Enter your Username: ")
+        password = input("Enter your password: ")
+        confirm = input("Confirm your password: ")
+        if password == confirm:
+            auth.create_user_with_email_and_password(email, password)
+            print("\nYou have been signed up")
+            print(f"Welcome to SkillSync {email}")
+            break
+        else:
+            print("Passwords do not match\nTry Again\n")
